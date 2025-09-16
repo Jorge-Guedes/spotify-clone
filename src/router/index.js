dd4980a81735a6b45c584b4240b865d11ea73e2a
Jorge-Guedes <jorge.guedes.pena@gmail.com>
2025-09-16 23:56:43 +0100

    Created HomeView and HomeCare components and added HomeCare elements to HomeView

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,13 @@
 import { createWebHistory, createRouter } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.BASE_URL),
   routes: [
     {
       path: "/",
+      name: "home",
+      component: HomeView,
     },
   ],
 });
