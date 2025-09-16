15d648ee06cfd1b87a96ac588a4c4b414e5f088b
Jorge-Guedes <jorge.guedes.pena@gmail.com>
2025-09-16 22:50:05 +0100

    MenuItem elements added to SideNav

--- src/App.vue
+++ src/App.vue
@@ -60,6 +60,86 @@ let openMenu = ref(false);
         </ul>
       </span>
     </div>
-    <div id="SideNav" class="h-[100%] p-6 w-[240px] fixed z-50 bg-black"></div>
+    <div id="SideNav" class="h-[100%] p-6 w-[240px] fixed z-50 bg-black">
+      <RouterLink to="/">
+        <img width="125" src="/images/icons/spotify-logo.png" />
+      </RouterLink>
+      <div class="my-8"></div>
+      <ul>
+        <RouterLink to="/">
+          <MenuItem
+            class="ml-[1px]"
+            :icon-size="23"
+            name="Home"
+            icon-string="home"
+            page-url="/"
+          />
+        </RouterLink>
+        <RouterLink to="/search">
+          <MenuItem
+            class="ml-[1px]"
+            :icon-size="23"
+            name="Search"
+            icon-string="search"
+            page-url="/search"
+          />
+        </RouterLink>
+        <RouterLink to="/library">
+          <MenuItem
+            class="ml-[1px]"
+            :icon-size="23"
+            name="Library"
+            icon-string="library"
+            page-url="/library"
+          />
+        </RouterLink>
+        <div class="py-3.5"></div>
+        <MenuItem
+          class="ml-[1px]"
+          :icon-size="24"
+          name="Create Playlist"
+          icon-string="playlist"
+          page-url="/playlist"
+        />
+        <MenuItem
+          class="ml-[1px]"
+          :icon-size="24"
+          name="Liked Song"
+          icon-string="liked"
+          page-url="/liked"
+        />
+      </ul>
+      <div class="border-b border-b-gray-700"></div>
+      <ul>
+        <li
+          class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white"
+        >
+          My Playlist #1
+        </li>
+        <li
+          class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white"
+        >
+          My Playlist #2
+        </li>
+        <li
+          class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white"
+        >
+          My Playlist #3
+        </li>
+        <li
+          class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white"
+        >
+          My Playlist #4
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  <div
+    class="fixed right-0 top-0 w-[calc(100%-240px)] overflow-auto h-full bg-gradient-to-b from-[#1C1C1C] to-black"
+  >
+    <div class="mt-[70px]"></div>
+    <RouterView />
+    <div class="mb-[100px]"></div>
   </div>
 </template>
