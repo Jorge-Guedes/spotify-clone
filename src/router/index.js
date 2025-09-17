266e8628b8188d108e7757558cea6916cd10f43e
Jorge-Guedes <jorge.guedes.pena@gmail.com>
2025-09-17 20:47:15 +0100

    Creating the SearchView and the CategorySelect component. CategorySelect component logic. Calling CategorySelect from a SearchView, displaying data obtained from a created JSON file of categories.

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,17 @@
 import { createWebHistory, createRouter } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import SearchView from "../views/SearchView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "home",
       component: HomeView,
+    },
+    {
+      path: "/search",
+      component: SearchView,
     },
   ],
 });
